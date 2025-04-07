7eefa412f8c6f0d7c7391076e3c078fd88340545
anay-208 <me@nayparaswani.dev>
2025-04-07 22:37:42 +0530

    Create schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,21 @@
-import { integer, pgTable, varchar, json } from "drizzle-orm/pg-core";
+import { pgTable, serial, text, numeric, timestamp, integer, varchar, json } from 'drizzle-orm/pg-core';
 
-export const users = pgTable("users", {
-  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
-  email: varchar("email", { length: 255 }).notNull().unique(),
-  name: json("name").notNull(), // JSON field for first and last name
-  password: varchar("password", { length: 255 }).notNull(),
+const timestamps = {
+    created_at: timestamp().defaultNow().notNull(),
+  }
+
+export const usersTable = pgTable("users", {
+  id: integer("id").primaryKey(),
+  email: text("email").notNull().unique(),
+  name: json("name").notNull(),
+  password: text("password").notNull(),
+  ...timestamps,
 });
+
+export const expenseTable = pgTable('expense', {
+    id: serial('id').primaryKey(),
+    description: text('description').notNull(),
+    amount: numeric('amount', { precision: 10, scale: 2 }).notNull(),
+    date: timestamp('date').defaultNow().notNull(),
+    ...timestamps
+  });
