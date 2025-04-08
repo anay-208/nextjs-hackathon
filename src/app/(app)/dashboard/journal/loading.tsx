a8b8265283f1ef083db806fad898d80bb5e42334
Arinji2 <arinjaydhar205@gmail.com>
2025-04-08 15:28:44 +0530

    Finish Base Journal Homepage

--- src/app/(app)/dashboard/journal/loading.tsx
+++ src/app/(app)/dashboard/journal/loading.tsx
@@ -1,3 +1,16 @@
-export default function Loading() {
-  return <div>Loading...</div>;
+import { JournalCreateCard, JournalLoadingCard } from "./card";
+import { JournalDashboardSize } from "./constants";
+export default async function Loading() {
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-start gap-5 py-10">
+      <h1 className="text-4xl font-bold">My Journal</h1>
+
+      <div className="grid h-full w-full flex-1 grid-cols-1 gap-5 px-4 md:grid-cols-2 lg:grid-cols-3">
+        <JournalCreateCard inactive />
+        {Array.from({ length: JournalDashboardSize }).map((_, i) => (
+          <JournalLoadingCard key={i} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/app/(app)/dashboard/journal/page.tsx
+++ src/app/(app)/dashboard/journal/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <div>Journalling</div>;
+import { getAllData } from "./[id]/data";
+import { JournalCard, JournalCreateCard } from "./card";
+import "./[id]/tiptap.css";
+import { JournalDashboardSize } from "./constants";
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    pageNumber: string;
+  }>;
+}) {
+  let page = 1;
+  const { pageNumber } = await searchParams;
+  if (pageNumber) {
+    page = parseInt(pageNumber);
+  }
+  const data = await getAllData();
+  const paginated = data.slice(
+    (page - 1) * JournalDashboardSize,
+    page * JournalDashboardSize,
+  );
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center justify-start gap-5 py-10">
+      <h1 className="text-4xl font-bold">My Journal</h1>
+      <div className="grid h-full w-full flex-1 grid-cols-1 gap-5 px-4 md:grid-cols-2 lg:grid-cols-3">
+        <JournalCreateCard />
+
+        {paginated.map((journal) => (
+          <JournalCard key={journal.id} data={journal} />
+        ))}
+      </div>
+    </div>
+  );
 }
