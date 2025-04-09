bd2c970793fe2989303351f4a58b2d8a9968ef16
anay-208 <me@nayparaswani.dev>
2025-04-09 08:00:40 +0530

    fix conflicts

--- src/app/(app)/auth/auth-client.ts
+++
@@ -1,3 +0,0 @@
-import { createAuthClient } from "better-auth/react"
-// has methods like signIn, signUp
-export const authClient = createAuthClient()
---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import SignInPage from "./page.client";
+
+
+export default function Page(){
+  // TODO: Check session exists and redirect to dashboard
+
+  return (
+    <SignInPage />
+  )
+}
