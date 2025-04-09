5930230608b3144ae7db4bdbdee0984043bbe595
anay-208 <me@nayparaswani.dev>
2025-04-09 08:04:57 +0530

    fix conflicts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,17 @@
 
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(process.env.DATABASE_URL!);
-
 import * as schema from "@/db/schema";
 import * as dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
-import { drizzle } from "drizzle-orm/node-postgres";
 
 dotenvExpand.expand(
   dotenv.config({
     path: [".env.development.local", ".env.production.local", ".env.local"],
   }),
 );
+
+
 
 export function getDbConnectionString() {
   return process.env.DATABASE_URL!;
