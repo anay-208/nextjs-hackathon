efca3a82652773ec61a0320d4134f0f40f214e0f
Arinji2 <arinjaydhar205@gmail.com>
2025-04-10 11:11:58 +0530

    Update Journal Homepage

--- src/app/(app)/dashboard/journal/page.tsx
+++ src/app/(app)/dashboard/journal/page.tsx
@@ -2,7 +2,9 @@ import { getAllData } from "./[id]/data";
 import { JournalCard, JournalCreateCard } from "./card.client";
 import "./[id]/tiptap.css";
 import { JournalDashboardSize } from "./constants";
-import { Pagination } from "./pagination";
+import { Pagination, PaginationLoading } from "./pagination";
+import { getJournalCount, listJournals } from "@/app/api/journal/actions";
+import { Suspense } from "react";
 export default async function Page({
   searchParams,
 }: {
@@ -10,27 +12,26 @@ export default async function Page({
     pageNumber: string;
   }>;
 }) {
-  let page = 1;
+  let page = 0;
   const { pageNumber } = await searchParams;
   if (pageNumber) {
     page = parseInt(pageNumber);
   }
-  const data = await getAllData();
-  const paginated = data.slice(
-    (page - 1) * JournalDashboardSize,
-    page * JournalDashboardSize,
-  );
-  const totalPages = Math.ceil(data.length / JournalDashboardSize);
+  const dataRes = await listJournals({
+    page: page,
+  });
+  const data = dataRes.data ?? [];
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-start gap-5 py-10">
       <h1 className="text-4xl font-bold">My Journal</h1>
-
-      <Pagination currentPage={page} totalPages={totalPages} />
+      <Suspense fallback={<PaginationLoading />}>
+        <Pagination currentPage={page} />
+      </Suspense>
       <div className="grid h-full w-full flex-1 grid-cols-1 gap-5 px-4 md:grid-cols-2 lg:grid-cols-3">
         <JournalCreateCard />
 
-        {paginated.map((journal) => (
-          <JournalCard key={journal.id} data={journal} />
+        {data.map((d) => (
+          <JournalCard key={d.id} data={d} />
         ))}
       </div>
     </div>
--- src/app/(app)/dashboard/journal/pagination.tsx
+++ src/app/(app)/dashboard/journal/pagination.tsx
@@ -1,26 +1,25 @@
+import { getJournalCount } from "@/app/api/journal/actions";
 import { cn } from "@/lib/utils";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
+import { JournalDashboardSize } from "./constants";
 
-export function Pagination({
-  currentPage,
-  totalPages,
-}: {
-  currentPage: number;
-  totalPages: number;
-}) {
+export async function Pagination({ currentPage }: { currentPage: number }) {
+  const countRes = await getJournalCount();
+  let count = countRes.data ?? 0;
+  const totalPages = Math.ceil(count / JournalDashboardSize);
   return (
     <div className="flex flex-row items-center justify-center gap-4">
       <Link
         href={
-          currentPage - 1 === 0
+          currentPage - 1 < 0
             ? ""
             : `/dashboard/journal?pageNumber=${currentPage - 1}`
         }
         scroll={false}
         aria-label="Previous page"
         className={cn("border-muted-foreground border-2 p-1", {
-          "cursor-not-allowed opacity-50": currentPage === 1,
+          "cursor-not-allowed opacity-50": currentPage - 1 < 0,
         })}
       >
         <ChevronLeft
@@ -28,9 +27,13 @@ export function Pagination({
           strokeWidth={2}
         />
       </Link>
-      <p>{currentPage}</p>
+      <p>{currentPage + 1}</p>
       <Link
-        href={`/dashboard/journal?pageNumber=${currentPage + 1}`}
+        href={
+          currentPage === totalPages
+            ? ""
+            : `/dashboard/journal?pageNumber=${currentPage + 1}`
+        }
         scroll={false}
         className={cn("border-muted-foreground border-2 p-1", {
           "cursor-not-allowed opacity-50": currentPage === totalPages,
@@ -42,6 +45,26 @@ export function Pagination({
           strokeWidth={2}
         />
       </Link>
+    </div>
+  );
+}
+
+export function PaginationLoading() {
+  return (
+    <div className="flex flex-row items-center justify-center gap-4">
+      <div className="border-muted-foreground cursor-not-allowed border-2 p-1 opacity-50">
+        <ChevronLeft
+          className="text-accent-foreground size-5"
+          strokeWidth={2}
+        />
+      </div>
+      <p>{1}</p>
+      <div className="border-muted-foreground cursor-not-allowed border-2 p-1 opacity-50">
+        <ChevronRight
+          className="text-accent-foreground size-5"
+          strokeWidth={2}
+        />
+      </div>
     </div>
   );
 }
