9187796f5dd764dfe6a7fcf0d667902af496a6fc
b33fb0n3 <b33fb0n3yt@gmail.com>
2025-04-10 19:33:18 +0200

    Fix ts errors

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -1,14 +1,18 @@
-"use client"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+"use client";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
 
 interface DatePickerProps {
-  date: Date 
-  setDate: (date: Date ) => void
+  date: Date;
+  setDate: (date: Date) => void;
 }
 
 export function DatePicker({ date, setDate }: DatePickerProps) {
@@ -17,15 +21,24 @@ export function DatePicker({ date, setDate }: DatePickerProps) {
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
-          className={cn("w-full justify-start text-left font-normal", !date && "text-muted-foreground")}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
           {date ? format(date, "PPP") : <span>Pick a date</span>}
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
-        <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          autoFocus
+          required
+        />
       </PopoverContent>
     </Popover>
-  )
+  );
 }
