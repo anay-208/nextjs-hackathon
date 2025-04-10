381e422be345e72082549c2e2f6e3ca8bf794a25
Alfonsus Ardani <alfonsusac@gmail.com>
2025-04-10 18:14:10 +0700

    fix: Type error: Page "src/app/(app)/dashboard/page.tsx" does not match the required types of a Next.js Page.

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -91,7 +91,7 @@ function MaterialSymbolsLocalFireDepartmentRounded(props: SVGProps<SVGSVGElement
 }
 
 
-export function MaterialSymbolsAsterisk(props: SVGProps<SVGSVGElement>) {
+function MaterialSymbolsAsterisk(props: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>{/* Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE */}<path fill="currentColor" d="M11 21v-6.6l-4.65 4.675l-1.425-1.425L9.6 13H3v-2h6.6L4.925 6.35L6.35 4.925L11 9.6V3h2v6.6l4.65-4.675l1.425 1.425L14.4 11H21v2h-6.6l4.675 4.65l-1.425 1.425L13 14.4V21z"></path></svg>
   )
