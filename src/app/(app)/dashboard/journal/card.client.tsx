77382a2b8426e6f6b4055c50c60f52cc3db6a40c
Arinji2 <arinjaydhar205@gmail.com>
2025-04-10 11:11:25 +0530

    Update Create Journal
    Install unique-names-generator

--- src/app/(app)/dashboard/journal/card.client.tsx
+++ src/app/(app)/dashboard/journal/card.client.tsx
@@ -1,10 +1,21 @@
+"use client";
+import { createJournal } from "@/app/api/journal/actions";
 import { SelectJournalType } from "@/app/api/journal/types";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { PlusCircle } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
 
-export function JournalCard({ data }: { data: SelectJournalType }) {
+type JournalWithoutContent = Omit<SelectJournalType, "content">;
+export function JournalCard({ data }: { data: JournalWithoutContent }) {
   return (
     <Link
       href={`/dashboard/journal/${data.id}`}
@@ -34,10 +45,22 @@ export function JournalLoadingCard() {
 }
 
 export function JournalCreateCard({ inactive }: { inactive?: boolean }) {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
   return (
     <button
-      onClick={async () => {}}
-      disabled={inactive}
+      onClick={() => {
+        startTransition(async () => {
+          const generatedTitle = uniqueNamesGenerator({
+            dictionaries: [adjectives, colors, animals],
+            separator: "-",
+            style: "lowerCase",
+          });
+          await createJournal({ title: generatedTitle });
+          router.refresh();
+        });
+      }}
+      disabled={inactive || isPending}
       className="border-accent-foreground bg-accent relative flex h-[200px] w-full flex-col items-center justify-center gap-2 overflow-hidden rounded-md border-2 p-2 px-2 disabled:cursor-not-allowed disabled:opacity-50"
     >
       <PlusCircle className="text-muted-foreground size-6" />
