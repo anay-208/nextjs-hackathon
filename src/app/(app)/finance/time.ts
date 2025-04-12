36d65fa8e3825c5c7ae3fa264237888d86f0a073
Arinji2 <arinjaydhar205@gmail.com>
2025-04-12 11:30:39 +0530

    Update times utils

--- src/app/(app)/finance/time.ts
+++ src/app/(app)/finance/time.ts
@@ -1,3 +1,48 @@
+import { TimeRange } from "@/app/api/types";
+
+export const validTimes = [
+  "today",
+  "yesterday",
+  "this-week",
+  "last-week",
+  "this-month",
+  "last-month",
+  "this-year",
+  "last-year",
+  "all-time",
+] as const;
+
+export type Time = (typeof validTimes)[number];
+
+export const isValidTime = (value: any): value is Time => {
+  return validTimes.includes(value);
+};
+
+export const toReadableTime = (time: Time) => {
+  switch (time) {
+    case "today":
+      return "Today";
+    case "yesterday":
+      return "Yesterday";
+    case "this-week":
+      return "This Week";
+    case "last-week":
+      return "Last Week";
+    case "this-month":
+      return "This Month";
+    case "last-month":
+      return "Last Month";
+    case "this-year":
+      return "This Year";
+    case "last-year":
+      return "Last Year";
+    case "all-time":
+      return "All Time";
+    default:
+      return "Today";
+  }
+};
+
 const formatStartOfDay = (date: Date): string => {
   const d = new Date(date);
   d.setUTCHours(0, 0, 0, 0);
@@ -10,43 +55,101 @@ const formatEndOfDay = (date: Date): string => {
   return d.toISOString();
 };
 
-export const getTimeRanges = (allTimeStartDate?: Date) => {
+export const getTimeRange = (
+  range: Time,
+  allTimeStartDate?: Date,
+): TimeRange => {
   const now = new Date();
 
-  const todayStart = formatStartOfDay(now);
-  const todayEnd = formatEndOfDay(now);
+  switch (range) {
+    case "today": {
+      return {
+        startDate: formatStartOfDay(now),
+        endDate: formatEndOfDay(now),
+      };
+    }
 
-  const lastWeekStart = new Date(now);
-  lastWeekStart.setDate(now.getDate() - 7);
+    case "yesterday": {
+      const yesterday = new Date(now);
+      yesterday.setDate(now.getDate() - 1);
+      return {
+        startDate: formatStartOfDay(yesterday),
+        endDate: formatEndOfDay(yesterday),
+      };
+    }
 
-  const lastMonthStart = new Date(now);
-  lastMonthStart.setMonth(now.getMonth() - 1);
+    case "this-week": {
+      const start = new Date(now);
+      start.setDate(now.getDate() - now.getDay()); // sunday
+      return {
+        startDate: formatStartOfDay(start),
+        endDate: formatEndOfDay(now),
+      };
+    }
 
-  const lastYearStart = new Date(now);
-  lastYearStart.setFullYear(now.getFullYear() - 1);
+    case "last-week": {
+      const thisWeekStart = new Date(now);
+      thisWeekStart.setDate(now.getDate() - now.getDay());
+      const lastWeekStart = new Date(thisWeekStart);
+      lastWeekStart.setDate(thisWeekStart.getDate() - 7);
+      const lastWeekEnd = new Date(thisWeekStart.getTime() - 1);
+      return {
+        startDate: formatStartOfDay(lastWeekStart),
+        endDate: formatEndOfDay(lastWeekEnd),
+      };
+    }
 
-  return {
-    today: {
-      startDate: todayStart,
-      endDate: todayEnd,
-    },
-    lastWeek: {
-      startDate: formatStartOfDay(lastWeekStart),
-      endDate: todayEnd,
-    },
-    lastMonth: {
-      startDate: formatStartOfDay(lastMonthStart),
-      endDate: todayEnd,
-    },
-    lastYear: {
-      startDate: formatStartOfDay(lastYearStart),
-      endDate: todayEnd,
-    },
-    allTime: allTimeStartDate
-      ? {
-          startDate: formatStartOfDay(allTimeStartDate),
-          endDate: todayEnd,
-        }
-      : null,
-  };
+    case "this-month": {
+      const start = new Date(now.getFullYear(), now.getMonth(), 1);
+      return {
+        startDate: formatStartOfDay(start),
+        endDate: formatEndOfDay(now),
+      };
+    }
+
+    case "last-month": {
+      const start = new Date(now.getFullYear(), now.getMonth() - 1, 1);
+      const end = new Date(now.getFullYear(), now.getMonth(), 0);
+      return {
+        startDate: formatStartOfDay(start),
+        endDate: formatEndOfDay(end),
+      };
+    }
+
+    case "this-year": {
+      const start = new Date(now.getFullYear(), 0, 1);
+      return {
+        startDate: formatStartOfDay(start),
+        endDate: formatEndOfDay(now),
+      };
+    }
+
+    case "last-year": {
+      const start = new Date(now.getFullYear() - 1, 0, 1);
+      const end = new Date(now.getFullYear(), 0, 0);
+      return {
+        startDate: formatStartOfDay(start),
+        endDate: formatEndOfDay(end),
+      };
+    }
+
+    case "all-time": {
+      if (!allTimeStartDate)
+        return {
+          startDate: formatStartOfDay(now),
+          endDate: formatEndOfDay(now),
+        };
+      return {
+        startDate: formatStartOfDay(allTimeStartDate),
+        endDate: formatEndOfDay(now),
+      };
+    }
+
+    default:
+      console.error("Invalid time range, defaulting to today");
+      return {
+        startDate: formatStartOfDay(now),
+        endDate: formatEndOfDay(now),
+      };
+  }
 };
