0a38b5a9e72e53c29221c41a4075fce388e8ecd7
Arinji2 <arinjaydhar205@gmail.com>
2025-04-13 23:47:24 +0530

    Finish Finance Homepage

--- src/app/(app)/finance/page.tsx
+++ src/app/(app)/finance/page.tsx
@@ -3,44 +3,30 @@ import { isValidTime, Time } from "./time";
 import Amount from "./amount";
 import Presets from "./presets";
 import TransactionList from "./transaction";
+import Graph from "./graph";
+import TimeSelector from "./time.client";
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{
-    amountTime: string | undefined;
+    timeFrame: string | undefined;
   }>;
 }) {
-  let parsedAmountTime: Time = "today";
-  const { amountTime } = await searchParams;
-  if (amountTime && isValidTime(amountTime)) {
-    parsedAmountTime = amountTime;
+  let parsedTimeFrame: Time = "today";
+  const { timeFrame } = await searchParams;
+  if (timeFrame && isValidTime(timeFrame)) {
+    parsedTimeFrame = timeFrame;
   }
 
   return (
     <div className="flex min-h-[100svh] w-full flex-row items-center justify-between gap-10">
-      <div className="flex h-full w-[15%] shrink-0 flex-col items-start justify-start gap-10 border-2 border-red-500 px-2 py-4">
-        <Link className="text-lg font-bold text-black" href="/finance">
-          Summary
-        </Link>
-        <Link className="text-lg font-bold text-black" href="/finance/analysis">
-          Analysis
-        </Link>
-        <Link className="text-lg font-bold text-black" href="/finance/budgets">
-          Budgets and Plans
-        </Link>
-        <Link className="text-lg font-bold text-black" href="/finance/presets">
-          Presets
-        </Link>
-        <Link className="text-lg font-bold text-black" href="/finance/presets">
-          History
-        </Link>
-      </div>
-
       <div className="flex h-full w-full flex-col items-center justify-start gap-10">
         <h1 className="text-3xl font-bold text-black">Summary</h1>
-        <Amount amountTime={parsedAmountTime} />
+        <TimeSelector time={parsedTimeFrame} />
+        <Amount timeFrame={parsedTimeFrame} />
         <Presets />
-        <TransactionList />
+        <TransactionList timeFrame={parsedTimeFrame} />
+        <Graph timeFrame={parsedTimeFrame} />
       </div>
     </div>
   );
