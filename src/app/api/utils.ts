094a4dec5024c4feb8f024fc596c9238d85dacb1
b33fb0n3 <b33fb0n3yt@gmail.com>
2025-04-13 09:43:57 +0200

    refactor: update session retrieval to use auth.api and simplify user validation

--- src/app/api/utils.ts
+++ src/app/api/utils.ts
@@ -1,22 +1,12 @@
-import { authClient } from "@/auth/client";
-import { z } from "zod";
+import { auth } from "@/auth";
+import { headers } from "next/headers";
 import { APIResponse } from "./types";
 
 export const getValidUser = async () => {
-  const session = await authClient.getSession();
-  console.log("session", session);
-  if (!session || !session.data?.user) throw new Error("Unauthorized");
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session || !session.user) throw new Error("Unauthorized");
 
-  const parseResult = z.number().safeParse(session.data.user.id);
-  if (!parseResult.success) throw new Error("Invalid author ID");
-
-  return {
-    ...session.data,
-    user: {
-      ...session.data.user,
-      id: session.data.user.id,
-    },
-  };
+  return session;
 };
 
 export const withAuth = async <T>(
