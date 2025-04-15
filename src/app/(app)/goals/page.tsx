bd5405b681d3eb6dee58d89985e96067d53a7061
Alfonsus Ardani <alfonsusac@gmail.com>
2025-04-15 18:39:10 +0700

    improve serverAuth.protectedPage type

--- src/app/(app)/goals/page.tsx
+++ src/app/(app)/goals/page.tsx
@@ -1,25 +1,18 @@
-import { headers } from "next/headers";
 import GoalsPage from "./page.client";
-import { auth } from "@/auth";
-import { unauthorized } from "next/navigation";
-import { db } from "@/db";
-import { goalsTable } from "@/db/schema";
-import { eq } from "drizzle-orm";
 import { getGoals } from "@/actions/goals/actions";
+import { serverAuth } from "@/auth/actions";
 
-export default async function Goals(){
-    const session = await auth.api.getSession({
-        headers: await headers()
-    }) 
-    if(!session){
-        return unauthorized();
-    }
-    const goals = await getGoals()
-    if(goals.error || !goals.data)
-        return <div className="text-red-500">Error fetching goals</div>
-    return (
-        <>
-        <GoalsPage goalsList={goals.data} author={session.session.userId} />
-        </>
-    )
+export default async function Goals() {
+  const session = await serverAuth.protectedPage('/goals')
+
+  const goals = await getGoals()
+
+  if (goals.error || !goals.data)
+    return <div className="text-red-500">Error fetching goals</div>
+  
+  return (
+    <>
+      <GoalsPage goalsList={goals.data} author={session.session.userId} />
+    </>
+  )
 }
