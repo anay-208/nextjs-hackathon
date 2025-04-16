1d9b7d7042c5d61b67aa75b7273d2ea5aa6e523f
b33fb0n3 <b33fb0n3yt@gmail.com>
2025-04-16 19:57:37 +0200

    Add search functionality to transaction
    - Enhanced TransactionsFilter to include a query for searching transaction labels.
    - Updated dbListTransactions and dbGetTransactionsCount to support filtering by query.
    - Reorganized imports in page.tsx for better readability.

--- src/app/(app)/journal/page.tsx
+++ src/app/(app)/journal/page.tsx
@@ -1,12 +1,11 @@
-import { JournalCard, JournalCreateCard } from "./card.client";
-import "./[id]/tiptap.css";
-import { JournalDashboardSize } from "./constants";
-import { Pagination } from "./pagination";
 import { listJournals } from "@/actions/journal/actions";
 import { Suspense } from "react";
 import { AppContent, PageLocation, PageTitle } from "../content-layouts";
+import "./[id]/tiptap.css";
+import { JournalCard, JournalCreateCard } from "./card.client";
+import { JournalDashboardSize } from "./constants";
+import { Pagination } from "./pagination";
 import { SearchJournalForm, SearchJournalInput } from "./search.client";
-
 
 export default function Page(props: {
   searchParams: Promise<{
@@ -14,7 +13,6 @@ export default function Page(props: {
     search: string;
   }>;
 }) {
-
   return (
     <AppContent>
       <PageLocation>Journal</PageLocation>
@@ -32,33 +30,35 @@ async function JournalPageList(props: {
     search: string;
   }>;
 }) {
-  let { pageNumber, search } = await props.searchParams;
+  const { pageNumber, search } = await props.searchParams;
   const journalList = await listJournals({
     page: pageNumber ? parseInt(pageNumber) : 0,
     pageSize: JournalDashboardSize,
     filter: {
       query: search ?? undefined,
-    }
+    },
   });
 
   if (!journalList.data) {
     return (
       <div className="flex h-full w-full items-center justify-center">
-        <p className="text-sm text-muted-foreground">No journals found</p>
+        <p className="text-muted-foreground text-sm">No journals found</p>
       </div>
     );
   }
 
   return (
     <SearchJournalForm>
-      <div className="pt-12 flex flex-col gap-4">
+      <div className="flex flex-col gap-4 pt-12">
         <div className="flex gap-2">
           <Pagination currentPage={parseInt(pageNumber)} />
           <SearchJournalInput />
         </div>
         <div className="grid h-full w-full flex-1 grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           <JournalCreateCard />
-          {journalList.data.map((d) => <JournalCard key={d.id} data={d} />)}
+          {journalList.data.map((d) => (
+            <JournalCard key={d.id} data={d} />
+          ))}
         </div>
       </div>
     </SearchJournalForm>
