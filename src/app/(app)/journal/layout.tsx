8db00debeedbdc9777f4fea50042344873130e65
Arinji2 <arinjaydhar205@gmail.com>
2025-04-18 10:49:05 +0530

    Fix Sidebar not being "sticky"

--- src/app/(app)/journal/layout.tsx
+++ src/app/(app)/journal/layout.tsx
@@ -1,17 +1,16 @@
 import { JournalSidebarClient } from "./sidebar.client";
 
 export default function Layout(props: {
-  children: React.ReactNode,
-  sidebar?: React.ReactNode,
+  children: React.ReactNode;
+  sidebar?: React.ReactNode;
 }) {
   return (
-    <div className="grow -m-(--p)  flex bg-main-4/13 min-h-0">
-      <JournalSidebarClient>
-        {props.sidebar}
-      </JournalSidebarClient>
-      <div className="grow bg-white p-(--p) rounded-lg  flex flex-col min-h-0 z-10">
+    <div className="bg-main-4/13 -m-(--p) flex min-h-0 grow">
+      <JournalSidebarClient>{props.sidebar}</JournalSidebarClient>
+      <div className="z-10 flex min-h-0 grow flex-col overflow-auto rounded-lg bg-white p-(--p)">
         {props.children}
       </div>
     </div>
   );
 }
+
